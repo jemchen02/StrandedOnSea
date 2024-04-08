4343ddd602e79ee373d8f8fa9fb80fa15ae41aaf
jlbilello <jlbilello@gmail.com>
2024-04-08 17:46:21 -0400

    torpedo tracks cursor

--- public/benchmark2/src/sos_project/AI/TorpedoAI.ts
+++ public/benchmark2/src/sos_project/AI/TorpedoAI.ts
@@ -2,6 +2,7 @@ import AI from "../../Wolfie2D/DataTypes/Interfaces/AI";
 import Vec2 from "../../Wolfie2D/DataTypes/Vec2";
 import Debug from "../../Wolfie2D/Debug/Debug";
 import GameEvent from "../../Wolfie2D/Events/GameEvent";
+import Input from "../../Wolfie2D/Input/Input";
 import GameNode from "../../Wolfie2D/Nodes/GameNode";
 import Graphic from "../../Wolfie2D/Nodes/Graphic";
 
@@ -27,7 +28,17 @@ export default class TorpedoAI implements AI {
 
     update(deltaT: number): void {
         if(!this.owner.visible) return;
-        this.owner.position.add((new Vec2(0, -1).rotateCCW(2 * Math.PI  - this.owner.rotation)).scaled(TorpedoAI.SPEED * deltaT).add(this.startingVelocity));
+
+        let mouseX = Input.getGlobalMousePosition().x;
+        let mouseY = Input.getGlobalMousePosition().y;
+
+        let deltaX = mouseX - this.owner.position.x;
+        let deltaY = mouseY - this.owner.position.y;
+        let angleToMouse = Math.atan2(deltaY, deltaX);
+
+        this.owner.rotation = angleToMouse;
+
+        this.owner.position.add((new Vec2(1, 0).rotateCCW(this.owner.rotation)).scaled(TorpedoAI.SPEED * deltaT).add(this.startingVelocity));
     }
 
     destroy(): void {
